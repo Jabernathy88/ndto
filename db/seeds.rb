c82677536eb2b939da751d9b92a5697333c6beb8
Jabernathy88 <j.e.abernathy@gmail.com>
2018-04-30 18:12:20 -0400

    Started building player seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MatchupPlayer.destroy_all
+Matchup.destroy_all
+Player.destroy_all
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+tourn1 = Tournament.find(1)
+tourn2 = Tournament.find(2) 
+
+team_black = Player.create(
+  tournament: tourn1,
+  name: "Team Black",
+  icon_img: 'black'
+)
+team_blue = Player.create(
+  tournament: tourn1,
+  name: "Team Blue",
+  icon_img: 'blue'
+)
+team_orange = Player.create(
+  tournament: tourn1,
+  name: "Team Green",
+  icon_img: 'green'
+)
+team_red = Player.create(
+  tournament: tourn1,
+  name: "Team Red",
+  icon_img: 'red'
+)
+
